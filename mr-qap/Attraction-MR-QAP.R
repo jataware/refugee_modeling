b4ffc2611c59ff10528522cffc24e1da38ad75b0
Brandon Rose <rose.brandon.m@gmail.com>
2022-05-27 17:19:34 -0500

    added additional features to mr-qap model

--- mr-qap/Attraction-MR-QAP.R
+++ mr-qap/Attraction-MR-QAP.R
@@ -57,6 +57,47 @@ libdem.mat <-
     )
   )
 
+migrant.mat <-
+  as.matrix(
+    as_adjacency_matrix(
+      igraph.edges,
+      type = "both",
+      names = T,
+      attr = "International.migrant.stock....of.population._norm"
+    )
+  )
+
+justice.mat <-
+  as.matrix(
+    as_adjacency_matrix(
+      igraph.edges,
+      type = "both",
+      names = T,
+      attr = "v2xeg_eqdr"
+    )
+  )
+
+trade.mat <-
+  as.matrix(
+    as_adjacency_matrix(
+      igraph.edges,
+      type = "both",
+      names = T,
+      attr = "trade_norm"
+    )
+  )
+
+pop.mat <-
+  as.matrix(
+    as_adjacency_matrix(
+      igraph.edges,
+      type = "both",
+      names = T,
+      attr = "Population..total_norm"
+    )
+  )
+
+
 ######## TEST DATA #######
 ##########################
 test.ref_pct_tot.mat <-
@@ -87,6 +128,46 @@ test.libdem.mat <-
     )
   )
 
+test.migrant.mat <-
+  as.matrix(
+    as_adjacency_matrix(
+      igraph.edges.test,
+      type = "both",
+      names = T,
+      attr = "International.migrant.stock....of.population._norm"
+    )
+  )
+
+test.justice.mat <-
+  as.matrix(
+    as_adjacency_matrix(
+      igraph.edges.test,
+      type = "both",
+      names = T,
+      attr = "v2xeg_eqdr"
+    )
+  )
+
+test.trade.mat <-
+  as.matrix(
+    as_adjacency_matrix(
+      igraph.edges.test,
+      type = "both",
+      names = T,
+      attr = "trade_norm"
+    )
+  )
+
+test.pop.mat <-
+  as.matrix(
+    as_adjacency_matrix(
+      igraph.edges.test,
+      type = "both",
+      names = T,
+      attr = "Population..total_norm"
+    )
+  )
+
 #####################################################
 # MRQAPs
 print("Fit MRQAPs")
@@ -97,7 +178,10 @@ ref_lm <-
     ref_pct_tot.mat,
     # Dependent variable/network
     list(gdp.mat,
-         libdem.mat),
+         libdem.mat,
+         migrant.mat,
+         justice.mat,
+         trade.mat),
     # List the independent variables/networks
     reps = 1000,
     nullhyp = "qapspp",
@@ -107,12 +191,16 @@ ref_lm <-
 
 preds <- ref_lm$coefficients[1] +
   ref_lm$coefficients[2] * test.gdp.mat +
-  ref_lm$coefficients[3] * test.libdem.mat
+  ref_lm$coefficients[3] * test.libdem.mat +
+  ref_lm$coefficients[4] * test.migrant.mat +
+  ref_lm$coefficients[5] * test.justice.mat + 
+  ref_lm$coefficients[6] * test.trade.mat
 
 preds <- data.frame(preds[1, 2:8])
-colnames(preds) <- c("pct_tot")
+colnames(preds) <- c("predicted_shares")
 preds <- cbind(country = rownames(preds), preds)
 rownames(preds) <- 1:nrow(preds)
-preds$pct_tot <- preds$pct_tot / sum(preds$pct_tot)
+preds$predicted_shares <- preds$predicted_shares / sum(preds$predicted_shares)
+print(preds)
 
-write.csv(preds[1, 2:8], file = 'mr-qap-results.csv')
+write.csv(preds, file = 'mr-qap-results.csv', row.names=FALSE)
